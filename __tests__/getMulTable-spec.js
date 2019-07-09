cb9e03c609ba81680ada780b254865297cfbd464
MurphyLee <36724748+MurphyLee2048@users.noreply.github.com>
2019-07-09 21:09:40 +0800

    commit getMulTable.js and getMulTable-spec.js

--- __tests__/getMulTable-spec.js
+++ __tests__/getMulTable-spec.js
@@ -1,6 +1,6 @@
-const getMulTable = require('./getMulTable');
+const getMulTable = require('../getMulTable');
 
-it('should return null given start 3 end 1', function() => {
+it('should return null when invoke isValid given start 3 end 1', function() {
     // given
     const start = 3;
     const end = 1;
@@ -10,4 +10,20 @@ it('should return null given start 3 end 1', function() => {
 
     // then
     expect(result).toBe(null);
+});
+
+it('should return 3x6 table when invoke createTable given start number 3 end number 6', function () {
+    // given
+    const start = 3;
+    const end = 6;
+
+    // when
+    // const result = getMulTable.createTable(start, end);
+
+    // then
+    expect(getMulTable.createTable(3, 6)).toBe("\n" +
+        "3*3=9\t\n" +
+        "3*4=12\t4*4=16\t\n" +
+        "3*5=15\t4*5=20\t5*5=25\t\n" +
+        "3*6=18\t4*6=24\t5*6=30\t6*6=36\t");
 });
--- getMulTable.js
+++ getMulTable.js
@@ -1,17 +1,17 @@
 function isValid(start, end) {
-    if (start < end ; start > 0 ; end < 1000) {
-    return 1;}
-    else {
-    return null;
+    if (start < end && start > 0 && end < 1000) {
+        return 1;
+    } else {
+        return null;
     }
 }
 
 function createTable(start, end) {
-    var str ='';
-    for (var i = start; i <= end; i++){
-    str = str + '\n';
-        for (var j = start; j <= i; j++){
-            str = str + j + "*" + i + "=" + j*i + '\t';
+    let str = '';
+    for (var i = start; i <= end; i++) {
+        str = str + '\n';
+        for (var j = start; j <= i; j++) {
+            str = str + j + "*" + i + "=" + j * i + '\t';
 //            str = str + '\t';
         }
     }
@@ -20,6 +20,6 @@ function createTable(start, end) {
 }
 
 module.exports = {
-    isValid:isValid,
-    createTable:createTable
-}
+    isValid: isValid,
+    createTable: createTable
+};
